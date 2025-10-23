58b85f23e19668da98a49987b02f9203851cda25
RSeldon06 <reinerseldon.delacerna.20@usjr.edu.ph>
2025-10-23 21:19:45 +0800

    Initial commit

---
+++ c.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int num1, num2, sum;
+
+    printf("input num1");
+    scanf("%d", &num1);
+    printf("input num2");
+    scanf("%d", &num2);
+
+    sum = num1 + num2;
+
+    printf("Sum is %d", sum);
+
+    return 0;
+}
